1d9ad75faae00b835f148da00d3b4f119a6b5981
alexandrejaguar <siqueiraaf@gmail.com>
2015-09-14 15:24:38 -0300

    Adicionando 2015_ieeeunicamp.

---
+++ 2015/ieeeunicamp/curso_dia5.m
@@ -0,0 +1,32 @@
+%%
+%% CURSO_DIA5.M
+%%
+%% Material de apoio à apostila "MATLAB e GNU Octave: guia de referência" 
+%% do minicurso "Introdução ao MATLAB", ministrado a pedido do Ramo 
+%% Estudantil IEEE da Unicamp em setembro de 2015.
+%%
+%% * Autor: Alexandre 'Jaguar' Fioravante de Siqueira
+%% * Contato: http://www.programandociencia.com/sobre/
+%%
+%% * Para citar esse material, por favor utilize a referência abaixo:
+%% DE SIQUEIRA, A.F.; MACHADO, D.F.T. MATLAB e GNU Octave: guia de 
+%% referência. Campinas: Programando Ciência, 2015. Disponível em: 
+%% http://www.programandociencia.com/.
+%%
+%% Este programa é um software livre; você pode redistribuí-lo e/ou 
+%% modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+%% publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+%% Licença, ou qualquer versão posterior.
+%%
+%% Este programa é distribuído na esperança de que possa ser útil, 
+%% mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+%% a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+%% Licença Pública Geral GNU para maiores detalhes.
+%%
+%% Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+%% com este programa. Se não, veja <http://www.gnu.org/licenses/>.
+%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% DIA 5: SCRIPTS E FUNÇÕES %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
